205ecd373725323fc2bc8614ec8e6f70e64e066d
keke0813 <149664866+keke0813@users.noreply.github.com>
2023-12-08 06:39:42 +0000

    bookのdestroy機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,8 +21,16 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
   
-  def 
-    
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path(book.id)
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
   
   private
